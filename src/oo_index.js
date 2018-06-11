415c4d4e0e18607e1ac85e65e70bb497c62f9490
SnowSurfer <drinkaziu@gmail.com>
2018-06-11 17:48:34 -0400

    bare minimum

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,26 @@
 
 document.addEventListener("DOMContentLoaded", () => {
   const taskList = new TaskList();
+
+  let taskForm = document.getElementById('create-task-form')
+  let newTaskDescription = document.getElementById('new-task-description')
+  let tasksUl = document.getElementById('tasks');
+  let button = document.getElementById('button');
+
+  button.addEventListener('click', handleButtonClick)
+
+  function createButton() {
+    let deleteButton = document.createElement('button');
+    deleteButton.innerText = 'x'
+    newTaskDescription.append(deleteButton);
+  }
+
+
+  function handleButtonClick(e) {
+    e.preventDefault();
+    let li = document.createElement('li');
+    li.innerText = newTaskDescription.value;
+    tasksUl.append(li);
+  }
+
 });
